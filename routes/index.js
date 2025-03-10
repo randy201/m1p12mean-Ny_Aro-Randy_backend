8f6ce1b1a98ab90d3b3c21813fef39a6fbfaec9a
nyarodina <nyarodina@gmail.com>
2025-03-10 13:17:47 +0300

    Merge branch 'main'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.status(200).send("HELLO WORLD"); // Use 200 for successful retrieval, not 500
+
 });
 
 module.exports = router;
