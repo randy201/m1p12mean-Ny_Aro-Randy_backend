03f82be86972ec4d4da893dd5d134589c59fada9
randy201 <108166894+randy201@users.noreply.github.com>
2025-03-10 20:46:18 +0300

    Squashed commit of the following:
    commit 8f6ce1b1a98ab90d3b3c21813fef39a6fbfaec9a
    Author: nyarodina <nyarodina@gmail.com>
    Date:   Mon Mar 10 13:17:47 2025 +0300
    
        Merge branch 'main'

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.status(200).send("HELLO WORLD"); // Use 200 for successful retrieval, not 500
+
 });
 
 module.exports = router;
