02d62ecd946bdf3fc023bd01b7b0cec7ca57c799
nyarodina <nyarodina@gmail.com>
2025-03-17 15:09:17 +0300

    Squashed commit of the following:
    commit 83e63f7f5f398ed7485ca8e4bee70460819856bb
    Author: randy201 <108166894+randy201@users.noreply.github.com>
    Date:   Mon Mar 10 20:58:32 2025 +0300
    
        Merge branch 'develop'
    
    commit 3e2b46073c51133648a2d1c053eb6c63014719cf
    Author: randy <108166894+randy201@users.noreply.github.com>
    Date:   Fri Mar 7 09:45:03 2025 +0300
    
        Update vercel.json
    
    commit fa690512e1442f787f3f791dad5a584b27f95f3a
    Author: randy <108166894+randy201@users.noreply.github.com>
    Date:   Fri Mar 7 09:38:53 2025 +0300
    
        Update vercel.json
    
    commit afa6418c2607f3d4c29b1a9d7d81a1c487105320
    Author: randy <108166894+randy201@users.noreply.github.com>
    Date:   Fri Mar 7 09:38:38 2025 +0300
    
        Update vercel.json
    
    commit 6987bafb3f922f37c976b68f03191d9e345cb4a5
    Author: randy <108166894+randy201@users.noreply.github.com>
    Date:   Fri Mar 7 09:20:22 2025 +0300
    
        Create vercel.json
    
    commit 9609f4f92512e5f5d434b153fa1439cd5348fb52
    Author: randy <108166894+randy201@users.noreply.github.com>
    Date:   Fri Mar 7 09:19:37 2025 +0300
    
        Create vercel.json
    
    commit 2c9420b079d605ca8233ee94639f0f47ba9053d3
    Author: randy <108166894+randy201@users.noreply.github.com>
    Date:   Fri Mar 7 09:11:25 2025 +0300
    
        Update index.js

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+var createError = require("http-errors");
+var environnement = require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -46,9 +55,40 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(logger("dev"));
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_CONNECT, 
+      {
+      authSource: "admin", // Often needed for authentication
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD
+    }
+  );
+    console.log("Connexion réussie à la base de données");
+  } catch (error) {
+    console.error("Erreur lors de la connexion à la base de données:", error);
+    process.exit(1); // Quitte le processus si la connexion échoue
+  }
+}
+
+// Appeler la fonction de connexion
+connectDB();
+
+// Rate limiter configuration
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later'
+});
+
+// Apply rate limiter to all routes
+app.use(limiter);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Security middleware
@@ -74,17 +114,21 @@ app.use("/email", emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
+  res.send("error");
   res.send("error");
 });
 
